627a70647056bc9136a0eedcc60af6b325ea6de1
B33 <my@email.com>
2020-02-23 14:53:28 -0600

    Rename api_connections file and add comments for clarity

--- api_connection.py
+++ api_connection.py
@@ -2,11 +2,14 @@ from PIL import Image
 import requests
 import os
 import shutil
+
 def get_API_data(url, params):
+    # generic method to talk to a JSON api.
     data = requests.get(url = url, params = params).json()
     return data
 
 def get_hiking_data(lat, lon):
+    # method for getting the data on hiking trails
     key = os.environ.get('HIKING_KEY')
     url = 'https://www.hikingproject.com/data/get-trails'
 
@@ -17,6 +20,7 @@ def get_hiking_data(lat, lon):
     return hiking_data
 
 def get_weather_data(lat, lon):
+    # method for getting data on the weather
     key = os.environ.get('WEATHER_KEY')
     url = 'https://samples.openweathermap.org/data/2.5/forecast/daily'
 
@@ -27,6 +31,9 @@ def get_weather_data(lat, lon):
     return weather_data
 
 def get_map(lat, lon):
+    # method for getting a map. Does not use the get_API_data method as the responce is not JSON
+    # information on getting images from an API found here:
+        # www.dev2qa.com/how-to-download-image-file-from-url-use-python-requests-or-wget-module/
     key = os.environ.get('MAP_KEY')
     params = {'access_token' : key}
     url = f'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/{lon},{lat},10,0,0/600x600'
@@ -38,6 +45,8 @@ def get_map(lat, lon):
 
 """test_lat = 44.97997
 test_lon = -93.26384
+
+# The method used to open the map image. PIL must be imported to do this.
 
 map_img = Image.open(get_map(test_lat, test_lon))
 map_img.show()
